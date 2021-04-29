0343be84143879fbb80317803ac6b4ca5fdcecd1
nlmhung <nguyenlemanhhung@gmail.com>
2021-04-29 11:59:40 +0700

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,18 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.16.0"
 
-set :application, "test_deploy"
+set :application, "myapp"
 set :repo_url, "git@github.com:nlmhung/test_deploy.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/var/www/myapp/code"
+#set :deploy_to, "/var/www/myapp/code"
+set :deploy_to, "/home/deploy/#{fetch :application}"
 
+
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
 
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server '192.168.1.14', user: 'deploy', roles: %w{app db web}
+server '192.168.1.4', user: 'deploy', roles: %w{app db web}
 
 
 # role-based syntax
